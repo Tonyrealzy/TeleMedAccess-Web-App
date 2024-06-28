3c389771c37b8e6718bc7c64233d6d2b110ac0a1
isaaco24 <isaacadegboyega24@gmail.com>
2024-06-28 21:48:31 +0100

    FAQ files updated

--- tele-med-access-app/src/pages/LandingPage.js
+++ tele-med-access-app/src/pages/LandingPage.js
@@ -1,12 +1,13 @@
 import React from "react";
-import './styles/LandingPage.css';
-import PrimaryButton from '../components/Button/PrimaryButton';
+import "./styles/LandingPage.css";
+import PrimaryButton from "../components/Button/PrimaryButton";
 import { useNavigate } from "react-router-dom";
 import NavBar from "../components/Navbar/NavBar";
 import doctor from "../assets/hero/doctor.jpg";
 import About from "../components/About/About";
 import Features from "../components/Features/Features";
 import Teams from "../components/Teams/Teams";
+import FAQ from "../components/FAQ/Faq";
 
 const LandingPage = () => {
   const navigate = useNavigate();
@@ -17,28 +18,32 @@ const LandingPage = () => {
       <section className="section" id="home">
         <div className="hero-content">
           <h1 className="title">Welcome to TeleMedAccess-Web-App</h1>
-          <p className="description">Bridging the gap between healthcare and rural communities.</p>
-          <PrimaryButton onClick={() => navigate('/initSession')}>GET STARTED</PrimaryButton>
+          <p className="description">
+            Bridging the gap between healthcare and rural communities.
+          </p>
+          <PrimaryButton onClick={() => navigate("/initSession")}>
+            GET STARTED
+          </PrimaryButton>
         </div>
         <div className="hero-image-container">
           <img src={doctor} alt="Hero Pic" className="hero-pic" />
         </div>
       </section>
-      
+
       <section id="about">
         <About />
       </section>
-      
+
       <section id="features">
         <Features />
       </section>
-      
+
       <section id="teams">
         <Teams />
       </section>
-    
-      
-
+      <section id="faq">
+        <FAQ />
+      </section>
     </div>
   );
 };
