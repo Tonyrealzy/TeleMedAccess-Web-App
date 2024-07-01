8753ec0a17e470a1a3b3d20c63e5df59aaa91a44
brwngld <bern587@yahoo.com>
2024-07-01 21:41:58 +0000

    Users welcome sessions's page almost completed. Will update soon.

--- tele-med-access-app/src/pages/InitSessionPage.js
+++ tele-med-access-app/src/pages/InitSessionPage.js
@@ -5,6 +5,8 @@ import { useAuth } from "../AuthContext";
 import { useNavigate, Link } from "react-router-dom";
 import Logger from "../components/Logger";
 import PrimaryButton from "../components/Button/PrimaryButton";
+import "../pages/styles/InitSessionPage.css";
+import hearticon from "../assets/initsession/pic.jpg";
 
 const InitSessionPage = () => {
   const { startSession, sessionID, agreed, setAgreed } = useAuth();
@@ -54,27 +56,46 @@ const InitSessionPage = () => {
   };
 
   return (
-    <div className="page1">
+    <div className="InitSession">
       {agreed === true ? (
-        <div>
-          <PrimaryButton onClick={() => navigate(-1)}>Back</PrimaryButton>
-          <br></br>
-          <h3>Initiate Session-With-Doctor Page</h3>
-          <p>
-            This page is displayed if the user already has started a session
-            before now on their account.
-          </p>
-          <div>
+        <>
+        <span id="backbtn">
+            <PrimaryButton onClick={() => navigate(-1)}>Back</PrimaryButton>
+          </span>
+        <div className="top">
+        <div className="content">
+          <div className="session-text">
+            <h3 className="session-title">Welcome back, TEMA User</h3>
+            <p className="session-description">
+              Your Comprehensive Health Companion
+              At TEMA, we are dedicated to bringing quality healthcare to rural and underserved areas. 
+              Our app offers you personalized health insights, easy appointment scheduling, and access to your medical records—all in one place.
+            </p>
+            <span id="continue">
             <PrimaryButton onClick={() => navigate('/addSymptoms')}>
               Continue
             </PrimaryButton>
+          </span>
+          </div>
+          <div className="session-image">
+            <img src={hearticon} alt="hero" />
+            <div className="text-box">
+              <p>
+                HealthBridge provides the tools you need to manage your health effectively. From appointment scheduling to accessing your medical records, we have you covered.
+              </p>
+              <PrimaryButton onClick={() => navigate('/learnMore')}>Learn More</PrimaryButton>
+            </div>
           </div>
         </div>
+      </div>
+      </>
       ) : (
         <div>
-          <PrimaryButton onClick={() => navigate(-1)}>Back</PrimaryButton>
+           <span id="backbtn">
+            <PrimaryButton onClick={() => navigate(-1)}>Back</PrimaryButton>
+          </span>
           <br></br>
-          <h3>Initiate Session With Doctor Page</h3>
+          <h3>Are you a new user</h3>
           <section>
             <p>
               This page is displayed if the user is creating a session for the
