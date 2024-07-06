75cdf7fa49d51430deabc3bf46f51e7ca803e372
Tonyrealzy <umehobiarinze2@gmail.com>
2024-07-06 20:07:12 +0100

    Deleted the node_modules to allow for new configurations

--- tele-med-access-app/src/components/Logger.js
+++
@@ -1,9 +0,0 @@
-const Logger = (error) => {
-  if (process.env.NODE_ENV === "production") {
-    console.log("Logger is running in production mode; check sever logs!");
-  } else {
-    console.log(error);
-  }
-};
-
-export default Logger;
--- tele-med-access-app/src/screens/Confirmation/confirmation.styles.js
+++
@@ -1,7 +0,0 @@
-import { styled } from "styled-components";
-
-export const FormContainer = styled.form`
-  .input-container {
-    margin-bottom: 12px;
-  }
-`;
